99ff07c30cc5b1ca293f22870b350dcd48a8e357
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-02-01 00:59:19 +0530

    fix CORS

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -24,7 +24,7 @@ declare global {
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://taskflow-k0es.onrender.com"],
   credentials: true
 }));
 
