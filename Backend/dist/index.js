8397cf832efaec6d501e5f616901f8fa8bbfbecb
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-02-01 15:13:41 +0530

    prod cors

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -25,7 +25,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173", "https://taskflow-k0es.onrender.com"],
+    origin: ["http://localhost:5173", "https://taskflowbybeast.vercel.app"],
     credentials: true
 }));
 app.post("/api/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
